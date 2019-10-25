2a8680d21e9f9ae5f8f274646abc0fa36b90adf9
Kevin Herrera <1herrerake@gmail.com>
2019-10-25 12:17:32 -0400

    styled games as cards with BS. Added game time, kickoff and game name to the data being displayed

--- src/App.js
+++ src/App.js
@@ -14,9 +14,29 @@ class App extends Component {
         }
         return (
           <div className="container-fluid mt-5">
+            <div className="text-center"><h1>Week {this.state.week}</h1></div>
             {this.state.gameInfo.map(game =>(
-              <div className="row mt-1" key={game.gameSchedule.gameId}>
-                <div className="col-6 offset-3 text-center">{game.gameSchedule.visitorNickname} vs {game.gameSchedule.homeNickname}</div>
+                <div className="card" key={game.gameSchedule.gameId}>
+                <div className="card-body">
+                  <h5 className="card-title">{game.gameSchedule.visitorDisplayName} at {game.gameSchedule.homeDisplayName}</h5>
+                  <div>{game.gameSchedule.gameDate}</div>
+                  <div>Kickoff - {game.gameSchedule.gameTimeEastern} EST</div>
+                  <form className="mt-3">
+                    <h6>My Pick</h6>
+                    <div className="form-check">
+                        <label className="teamLabel">
+                            <input type="radio" name="react-tips" value="option1" checked={true} className="form-check-input"/>
+                            {game.gameSchedule.homeNickname}
+                        </label>
+                      </div>
+                      <div className="form-check">
+                        <label className="teamLabel" >
+                            <input type="radio" name="react-tips" value="option2" className="form-check-input"/>
+                            {game.gameSchedule.visitorNickname}
+                        </label>
+                      </div>
+                  </form>
+                </div>
               </div>
             ))}
           </div>
@@ -25,7 +45,8 @@ class App extends Component {
 
     state = {
         loading: null,
-        gameInfo: []
+        gameInfo: [],
+        week: null
     };
 
     async componentDidMount() {
@@ -34,10 +55,11 @@ class App extends Component {
         const data = await response.json();
         this.setState({
           gameInfo: data.gameScores,
-          loading: false
+          loading: false,
+          week: data.week
         });
 
-        console.log(data.gameScores);
+        console.log(data);
     }
 }
 
